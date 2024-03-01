2524b93ad16dcb790e6baa763c78ef3269e3b27c
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2024-03-01 23:58:35 +0100

    :hammer: windows installation improvements & version bump

--- install.ps1
+++ install.ps1
@@ -16,58 +16,84 @@ if ($env:PROCESSOR_ARCHITECTURE -eq 'AMD64') {
 }
 
 # Download URL Construction
-$version = "1.0-rc3"
+$version = "1.0"
 $base_url = "https://github.com/yusufcanb/tlm/releases/download"
 $download_url = "${base_url}/${version}/tlm_${version}_${os}_${arch}.exe"
 
+# Ollama check
+$ollamaHost = $env:OLLAMA_HOST
+if (-not $ollamaHost) {
+    $ollamaHost = "http://localhost:11434"
+}
 
-# Ollama check - For Windows, we'll assume Ollama is installed directly on the system
 try {
-    Invoke-WebRequest -Uri "http://localhost:11434" -UseBasicParsing -ErrorAction Stop | Out-Null
+    Invoke-WebRequest -Uri $ollamaHost -UseBasicParsing -ErrorAction Stop | Out-Null
 } catch {
     Write-Host "ERR: Ollama not found." -ForegroundColor red
+    Write-Host "If you have Ollama installed, please make sure it's running and accessible at $ollamaHost" -ForegroundColor red
+    Write-Host "or configure OLLAMA_HOST environment variable." -ForegroundColor red
     Write-Host ""
-    Write-Host "*** On Windows: ***" -ForegroundColor green
+    Write-Host ">>> If have Ollama on your system or network, you can set the OLLAMA_HOST like below;"
+    Write-Host "    `$env:OLLAMA_HOST` = 'http://localhost:11434'"
     Write-Host ""
-    Write-Host "Ollama can run with GPU acceleration inside Docker containers for Nvidia GPUs."
-    Write-Host "To get started using the Docker image, please follow these steps:"
     Write-Host ""
-    Write-Host "1. *** CPU only: ***" -ForegroundColor green
-    Write-Host "   docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama"
+    Write-Host ">>> If you don't have Ollama installed, you can install it using the following methods;"
     Write-Host ""
-    Write-Host "2. *** Nvidia GPU: ***" -ForegroundColor green
-    Write-Host "   docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama"
+    Write-Host "    *** Windows: ***" -ForegroundColor green
+    Write-Host "    Download instructions can be followed at the following link: https://ollama.com/download"
+    Write-Host ""
+    Write-Host "    *** Official Docker Images: ***" -ForegroundColor green
+    Write-Host ""
+    Write-Host "    Ollama can run with GPU acceleration inside Docker containers for Nvidia GPUs."
+    Write-Host "    To get started using the Docker image, please follow these steps:"
+    Write-Host ""
+    Write-Host "        1. *** CPU only: ***" -ForegroundColor green
+    Write-Host "            docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama"
+    Write-Host ""
+    Write-Host "        2. *** Nvidia GPU: ***" -ForegroundColor green
+    Write-Host "            docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama"
     Write-Host ""
     Write-Host ""
     Write-Host "Installation aborted." -ForegroundColor red
-    Write-Host "Please install Ollama using the methods above and try again." -ForegroundColor red
-    return -1
+    Write-Host "Please install or configure Ollama using the methods above and try again." -ForegroundColor red
+    return
+}
+
+# Create Application Directory
+$install_directory = "C:\Users\$env:USERNAME\AppData\Local\Programs\tlm"
+if (-not (Test-Path $install_directory)) {
+    New-Item -ItemType Directory -Path $install_directory | Out-Null
 }
 
 # Download the binary
 Write-Host "Downloading tlm version ${version} for ${os}/${arch}..."
 try {
-    Invoke-WebRequest -Uri $download_url -OutFile 'tlm.exe' -UseBasicParsing -ErrorAction Stop | Out-Null
+    Invoke-WebRequest -Uri $download_url -OutFile "$install_directory\tlm.exe" -UseBasicParsing -ErrorAction Stop | Out-Null
 } catch {
     Write-Error "Download failed. Please check your internet connection and try again."
     return -1
 }
 
-# Move to installation directory
-Write-Host "Installing tlm..."
-#try {
-#    Move-Item -Path 'tlm.exe' -Destination 'C:\Windows\Program Files\tlm\' -Force
-#} catch {
-#    Write-Error "Installation requires administrator permissions. Please elevate with rights and run the script again."
-#    exit 1
-#}
+# Add installation directory to PATH
+$env:Path += ";$install_directory"
 
-# Ollama deployment - specific to the original script, might need modification
+# Configure tlm to use Ollama
 try {
-    .\tlm.exe deploy
+    ."$install_directory\tlm.exe" config set llm.host $ollamaHost
+} catch {
+    Write-Error "tlm config set llm.host failed."
+    return 1
+}
+
+# Deploy tlm
+try {
+
+    ."$install_directory\tlm.exe" deploy
 } catch {
     Write-Error "tlm deploy failed."
     return 1
 }
 
-Write-Host "Type '.\tlm.exe help' to get started."
+Write-Host ""
+Write-Host "Installation completed successfully."
+Write-Host "Type 'tlm help' to get started."
