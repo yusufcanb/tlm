f6760ab887302063f8d58151cfc0b1f00edf0a41
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2024-03-02 03:19:37 +0100

    :adhesive_bandage: showstopper fix

--- suggest/suggest.go
+++ suggest/suggest.go
@@ -5,10 +5,11 @@ import (
 )
 
 type Suggest struct {
-	api       *ollama.Client
-	modelfile string
+	api           *ollama.Client
+	modelfile     string
+	modelfileName string
 }
 
 func New(api *ollama.Client, modelfile string) *Suggest {
-	return &Suggest{api: api, modelfile: modelfile}
+	return &Suggest{api: api, modelfile: modelfile, modelfileName: "suggest:7b"}
 }
