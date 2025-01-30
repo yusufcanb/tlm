27f8a8397249e99bbc19794e8424d764136e7090
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2025-01-30 17:10:38 +0100

    shell: remove checkpoint error types

--- pkg/shell/errors.go
+++ pkg/shell/errors.go
@@ -3,7 +3,3 @@ package shell
 import "errors"
 
 var ShellReadingUserDirErr = errors.New("error getting user's home directory")
-
-var CheckpointFileNotExistErr = errors.New("checkpoint file does not exist")
-var CheckpointFileUnmarshalErr = errors.New("error unmarshaling checkpoint data")
-var CheckpointFileWriteErr = errors.New("error writing checkpoint file")
