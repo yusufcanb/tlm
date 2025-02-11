b93215b084b554cb0f28f6c74b7e1f304aea8480
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2025-02-11 16:45:23 +0100

    35: build fix

--- pkg/app/hooks.go
+++ pkg/app/hooks.go
@@ -10,22 +10,25 @@ import (
 	"github.com/yusufcanb/tlm/pkg/shell"
 )
 
+func shouldCheckOllamaIsSetandUp(commandName string) bool {
+	switch commandName {
+	case "ask", "a", "explain", "e", "suggest", "s":
+		return true
+	default:
+		return false
+	}
+}
+
 func notFound(_ *cli.Context, _ string) {
 	fmt.Println(shell.Err() + " command not found.")
 	os.Exit(-1)
 }
 
 func beforeRun(o *ollama.Client) func(c *cli.Context) error {
-
-	// fmt.Println("----DEBUG----")
-	// fmt.Println(strings.Join(os.Args, " "))
-	// fmt.Println("----DEBUG----")
-
 	return func(c *cli.Context) error {
 		arg := c.Args().Get(0)
 
-		// If the command is not help or version, check if Ollama is set and up
-		if arg != "v" || arg != "version" || arg != "help" || arg != "h" {
+		if shouldCheckOllamaIsSetandUp(arg) {
 			var err error
 
 			err = shell.CheckOllamaIsSet()
